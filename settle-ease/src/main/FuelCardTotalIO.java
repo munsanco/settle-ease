68f9d3f22091a2ae1137ee3047dbe05fc9708543
munsanco <msanchez@inspiretmstech.com>
2023-06-03 09:30:53 -0400

    Precondition Post Conditions
    comments updated

--- settle-ease/src/main/FuelCardTotalIO.java
+++ settle-ease/src/main/FuelCardTotalIO.java
@@ -2,27 +2,35 @@ package main;
 import java.io.*;
 
 public class FuelCardTotalIO {
+			// Precondition: fuelReport is not null and filePath is a valid file path.
     public static void writeFuelReportToFile(FuelReport fuelReport, String filePath) throws IOException {
         try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
             objectOutputStream.writeObject(fuelReport);
+            // Postcondition: The FuelReport object is written to the specified file.
         }
     }
 
     public static FuelReport readFuelReportFromFile(String filePath) throws IOException, ClassNotFoundException {
+    		// Precondition: filePath is a valid file path.
         try (FileInputStream fileInputStream = new FileInputStream(filePath);
              ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
             return (FuelReport) objectInputStream.readObject();
+            // Postcondition: The FuelReport object is read from the specified file and returned.
         }
     }
 
     public static void writeBytesToFile(byte[] bytes, String filePath) throws IOException {
+    		// Precondition: bytes is not null and filePath is a valid file path.
+       
         try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
             fileOutputStream.write(bytes);
+            // Postcondition: The byte array is written to the specified file.
         }
     }
 
     public static byte[] readBytesFromFile(String filePath) throws IOException {
+    		// Precondition: filePath is a valid file path.
         try (FileInputStream fileInputStream = new FileInputStream(filePath);
              ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream()) {
             byte[] buffer = new byte[4096];
@@ -31,21 +39,25 @@ public class FuelCardTotalIO {
                 byteOutputStream.write(buffer, 0, bytesRead);
             }
             return byteOutputStream.toByteArray();
+            // Postcondition: The byte array is read from the specified file and returned.
         }
     }
 
     public static void writePrimitiveTypesAndStringsToFile(String filePath, int intValue, double doubleValue, boolean booleanValue, String stringValue) throws IOException {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        	// Precondition: filePath is a valid file path.
+    	try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
              DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)) {
             dataOutputStream.writeInt(intValue);
             dataOutputStream.writeDouble(doubleValue);
             dataOutputStream.writeBoolean(booleanValue);
             dataOutputStream.writeUTF(stringValue);
+            // Postcondition: The primitive types and strings are written to the specified file.
         }
     }
 
     public static void readPrimitiveTypesAndStringsFromFile(String filePath) throws IOException {
-        try (FileInputStream fileInputStream = new FileInputStream(filePath);
+    		// Precondition: filePath is a valid file path.
+    	try (FileInputStream fileInputStream = new FileInputStream(filePath);
              DataInputStream dataInputStream = new DataInputStream(fileInputStream)) {
             int intValue = dataInputStream.readInt();
             double doubleValue = dataInputStream.readDouble();
@@ -56,6 +68,7 @@ public class FuelCardTotalIO {
             System.out.println("Double value: " + doubleValue);
             System.out.println("Boolean value: " + booleanValue);
             System.out.println("String value: " + stringValue);
+            // Postcondition: The primitive types and strings are read from the specified file and printed.
         }
     }
 }
