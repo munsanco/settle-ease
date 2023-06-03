b6a1fb2c02ffb2ea3acd8c2b46be7302b0275b5f
munsanco <msanchez@inspiretmstech.com>
2023-06-03 16:42:56 -0400

    comments
    misc updates

--- settle-ease/src/main/PayrollReport.java
+++ settle-ease/src/main/PayrollReport.java
@@ -1,20 +1,23 @@
 package main;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PayrollReport<T> extends Report<T> {
+public class PayrollReport<T> extends Report<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private List<T> payrollData;
 
     public PayrollReport(T reportData) {
-        super(reportData); // Invoke the superclass constructor with the reportData parameter
+        super(reportData);
         payrollData = new ArrayList<>();
         // Preconditions: reportData should not be null
     }
 
     public void addPayrollData(T data) {
         payrollData.add(data);
-       // Preconditions: Data should not be null
+        // Preconditions: Data should not be null
     }
 
     @Override
